92d0ff4e01c22a8e57155b3c3fb695ac63d9b92e
VisaaganG <343505582@tdsb.ca>
2026-05-01 13:32:54 +0000

    Questions Finished

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,35 @@
+/**
+	* File: Lesson 4.4 - Boolean functions with loops
+	* Author: Visaagan Gunabalachandran
+	* Date Created: April 6, 2023
+	* Date Last Modified: May 1, 2026
+	*/
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+	public static boolean hasCapital(String word) {
+	
+    for (int i = 0; i < word.length(); i++) {
+        if (Character.isUpperCase(word.charAt(i))) {
+            return true;
+        }
+    }
+    return false;
+}
+	public static boolean isPrime(int num) {
+    if (num <= 1) {
+        return false;
+    }
+    for (int i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
 
-	}
+
+
+
+
 
 }
